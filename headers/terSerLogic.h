7bf2abe7b767fa0e565e40c8032f16540ac46de2
Karol Szykuła <karol.szykula@wp.pl>
2018-02-26 11:25:09 +0100

    Add files of new class TerSerLogic

---
+++ headers/terSerLogic.h
@@ -0,0 +1,80 @@
+#ifndef TERSERLOGIC_H
+#define TERSERLOGIC_H
+
+#include <boost/shared_ptr.hpp>
+#include <wx/timer.h>
+#include <vector>
+
+#include "../bufferedasyncserial/BufferedAsyncSerial.h"
+#include "../bufferedasyncserial/serialoptions.h"
+
+class TerSerLogic //: public wxEvtHandler
+{
+public:
+        TerSerLogic();
+        ~TerSerLogic();
+
+        //! A public member function, sets main SerialOptions object.
+        /*!
+			\param newSerialOptions passes new serial connection options.
+        */
+        void setSerialOptions(SerialOptions newSerialOptions);
+
+		//! A public member function, returns main SerialOptions object.
+        /*!
+			\return SerialOptions object.
+        */
+        SerialOptions getSerialOptions();
+
+        void connectToSerial();
+
+        void disconnectFromSerial();
+
+        void setRelativeMainFrame(boost::shared_ptr<wxFrame* > mainFrame);
+
+private:
+
+        //! A private member function, callback function for the AsyncSerial library.
+        /*!
+			\param data is a char array where received signs will be stored.
+			\param len is size_t variable, it stores array size.
+        */
+		void OnSerialRecived(const char * data, std::size_t len);	// serial callback function
+
+		//! A private event function, it reads checks every specified interval whether data arrived.
+        /*!
+			\param event is wxTimerEvent type.
+        */
+		void OnTimerSerialRead(wxTimerEvent  & event);
+
+		//! A private function, it allow checking whether serial connection is open.
+        /*!
+			\return bool type.
+        */
+        bool isSerialOpen();
+
+        //! A private function, it stops the serial read timer.
+        void stopSerialReadTimer();
+
+        //! A private function, it starts the serial read timer.
+        void startSerialReadTimer();
+
+
+
+        //! An wxTimer object for serial read sample interval.
+        wxTimer m_serialReadTimer;
+
+        //! An BufferedAsyncSerial object for serial device abstraction.
+		BufferedAsyncSerial m_serialConnection;
+
+		//! A string for received message.
+		std::string m_serialReceived;
+
+		//! An object which stores serial device options.
+		SerialOptions m_serialOptions;
+
+		boost::shared_ptr<wxFrame* > ptrMainFrame;
+
+};
+
+#endif // TERSERLOGIC_H
---
+++ src/terSerLogic.cpp
@@ -0,0 +1,88 @@
+#include "../headers/terSerLogic.h"
+
+#include <wx/msgdlg.h>
+
+TerSerLogic::TerSerLogic()
+{
+    //m_serialReadTimer.SetOwner( this );
+	//this->Connect( m_serialReadTimer.GetId(), wxEVT_TIMER, wxTimerEventHandler( TerSerLogic::OnTimerSerialRead ), NULL, this );
+}
+TerSerLogic::~TerSerLogic() {}
+
+void TerSerLogic::OnTimerSerialRead(wxTimerEvent  & event)
+{
+	if(m_serialConnection.isOpen() == true)
+	{
+		m_serialReceived = m_serialConnection.readString();
+		//! \todo TerminalTextCtrlWidget has to be changed to a textCtrlWidget to a parser object
+		//TerminalTextCtrlWidget->AppendText(serialReceived);
+	}
+}
+
+void TerSerLogic::setSerialOptions(SerialOptions newSerialOptions)
+{
+	m_serialOptions = newSerialOptions;
+}
+
+SerialOptions TerSerLogic::getSerialOptions()
+{
+	return m_serialOptions;
+}
+
+void TerSerLogic::connectToSerial()
+{/*
+    if(m_serialConnection.isOpen() == false)
+	{
+		try{
+
+		m_serialConnection.open(m_serialOptions.getPortName(), m_serialOptions.getBaudRate(), m_serialOptions.getParity(),
+							m_serialOptions.getDataBits(), m_serialOptions.getFlowControl(), m_serialOptions.getStopBits());
+		}catch(boost::system::system_error& e)
+		{
+		    std::string err(e.what());
+		}
+
+		if(m_serialConnection.isOpen() == true)   // if serial connection opened
+		{
+			m_serialReadTimer.Start(30, wxTIMER_CONTINUOUS);
+			//!\todo LabelConnectionStatus has to be changed to a label in connectionStatusPanel
+			//LabelConnectionStatus->SetLabel(wxT("CONNECTED"));
+		}else if(m_serialConnection.isOpen() == false)    // if serial connection failed to open
+		{
+
+		}
+
+	}else if(m_serialConnection.isOpen() == true)
+	{
+		try{
+
+		m_serialConnection.close();
+
+		}catch(boost::system::system_error& e)
+		{}
+
+		if(m_serialConnection.isOpen() == false)
+		{
+			m_serialReadTimer.Stop();
+            //!\todo LabelConnectionStatus has to be changed to a label in connectionStatusPanel
+			//LabelConnectionStatus->SetLabel(wxT("DISCONNECTED"));
+		}
+	}
+	*/
+}
+
+void TerSerLogic::disconnectFromSerial()
+{
+    connectToSerial();
+}
+
+void TerSerLogic::setRelativeMainFrame(boost::shared_ptr<wxFrame* > mainFrame)
+{
+    ptrMainFrame = mainFrame;
+}
+
+
+
+
+
+
